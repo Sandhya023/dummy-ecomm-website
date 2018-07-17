3ed8d1608f06384d470e44ee4ae06519ceea2302
Sandhya023 <yadavshndhya@gmail.com>
2018-07-17 09:22:45 +0530

    Added user file

--- providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Use App::register to register any service provider
- *
- * Please note you can use this file to load any other code you wish
- */
-
-
-App::register('Company\Package\PackageServiceProvider');
